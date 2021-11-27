51e5b02fd0566fe8155f286d95088df6684dafd5
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-27 07:50:21 -0300

    project initialized

---
+++ src/main/java/com/jamerson/aulaospringdevsuperior/AulaospringdevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.jamerson.aulaospringdevsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaospringdevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaospringdevsuperiorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamerson/aulaospringdevsuperior/AulaospringdevsuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamerson.aulaospringdevsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaospringdevsuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
