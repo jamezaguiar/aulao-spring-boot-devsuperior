6a926ca5a03d6f8eead055023373b06bbe54f126
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-27 10:05:20 -0300

    saving categories with CommandLineRunner

--- src/main/java/com/jamerson/aulaospringdevsuperior/AulaospringdevsuperiorApplication.java
+++ src/main/java/com/jamerson/aulaospringdevsuperior/AulaospringdevsuperiorApplication.java
@@ -1,13 +1,27 @@
 package com.jamerson.aulaospringdevsuperior;
 
+import com.jamerson.aulaospringdevsuperior.entities.Category;
+import com.jamerson.aulaospringdevsuperior.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AulaospringdevsuperiorApplication {
+public class AulaospringdevsuperiorApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AulaospringdevsuperiorApplication.class, args);
-	}
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(AulaospringdevsuperiorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(1L, "Eletronics");
+        Category cat2 = new Category(2L, "Books");
+        categoryRepository.save(cat1);
+        categoryRepository.save(cat2);
+    }
 }
